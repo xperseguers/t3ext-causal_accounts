e6281203dec5d376cc14df63e55f9a417d1f1816
xp <xp@23db177c-05d9-4b13-9957-46ca24d4451f>
2012-02-23 16:49:49 +0000

    [TASK] Update md5 checksums
    git-svn-id: https://svn.causal.ch/subversion/causal/website/extensions/causal_accounts/trunk@396 23db177c-05d9-4b13-9957-46ca24d4451f

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "causal_accounts".
 #
-# Auto generated 26-01-2012 16:25
+# Auto generated 23-02-2012 17:46
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"aee1";s:31:"class.tx_causalaccounts_eid.php";s:4:"7dfe";s:47:"class.tx_causalaccounts_synchronizationtask.php";s:4:"e2ac";s:16:"ext_autoload.php";s:4:"238c";s:21:"ext_conf_template.txt";s:4:"ee07";s:12:"ext_icon.gif";s:4:"57bc";s:17:"ext_localconf.php";s:4:"b22a";s:14:"doc/manual.sxw";s:4:"713b";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"c8e8";s:31:"class.tx_causalaccounts_eid.php";s:4:"a568";s:47:"class.tx_causalaccounts_synchronizationtask.php";s:4:"e2ac";s:26:"class.ux_tx_openid_sv1.php";s:4:"6895";s:16:"ext_autoload.php";s:4:"238c";s:21:"ext_conf_template.txt";s:4:"2739";s:12:"ext_icon.gif";s:4:"57bc";s:17:"ext_localconf.php";s:4:"49eb";s:14:"doc/manual.sxw";s:4:"32f4";}',
 );
 
 ?>
