fabb4d11c1a3e3c2fc58d2c8c945d7ef72940efb
xp <xp@23db177c-05d9-4b13-9957-46ca24d4451f>
2014-03-14 21:28:34 +0000

    [BUGFIX] Short OpenID authentication does not work with TYPO3 4.7
    git-svn-id: https://svn.causal.ch/subversion/causal/website/extensions/causal_accounts/trunk@468 23db177c-05d9-4b13-9957-46ca24d4451f

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "causal_accounts".
-#
-# Auto generated 16-05-2013 09:01
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "causal_accounts".
+ *
+ * Auto generated 07-08-2013 09:44
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Central Account Management by Causal Sàrl',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"d78d";s:31:"class.tx_causalaccounts_eid.php";s:4:"dff2";s:47:"class.tx_causalaccounts_synchronizationtask.php";s:4:"9334";s:26:"class.ux_tx_openid_sv1.php";s:4:"c382";s:16:"ext_autoload.php";s:4:"238c";s:21:"ext_conf_template.txt";s:4:"e480";s:12:"ext_icon.gif";s:4:"57bc";s:17:"ext_localconf.php";s:4:"49eb";s:14:"doc/manual.sxw";s:4:"9806";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"8ac8";s:16:"ext_autoload.php";s:4:"5bc9";s:21:"ext_conf_template.txt";s:4:"e480";s:12:"ext_icon.gif";s:4:"57bc";s:17:"ext_localconf.php";s:4:"34c8";s:42:"Classes/4x/class.tx_causalaccounts_eid.php";s:4:"0cab";s:58:"Classes/4x/class.tx_causalaccounts_synchronizationtask.php";s:4:"54c9";s:37:"Classes/4x/class.ux_tx_openid_sv1.php";s:4:"3fcf";s:36:"Classes/Controller/EidController.php";s:4:"a5ef";s:36:"Classes/Task/SynchronizationTask.php";s:4:"8c13";s:32:"Classes/Xclass/OpenidService.php";s:4:"93d3";s:14:"doc/manual.sxw";s:4:"9777";}',
 );
 
 ?>
