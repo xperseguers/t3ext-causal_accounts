cd2a03cf7012a70b9abadf106e9e475fcf7d7cae
Xavier Perseguers <xavier@causal.ch>
2015-01-13 09:07:38 +0100

    [TASK] Raise version to 1.6.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "causal_accounts".
  *
- * Auto generated 09-09-2014 12:50
+ * Auto generated 13-01-2015 09:03
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.6.0-dev',
+	'version' => '1.6.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.3-5.6.99',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:16:"ext_autoload.php";s:4:"5bc9";s:21:"ext_conf_template.txt";s:4:"f711";s:12:"ext_icon.gif";s:4:"57bc";s:17:"ext_localconf.php";s:4:"34c8";s:42:"Classes/4x/class.tx_causalaccounts_eid.php";s:4:"5e7b";s:58:"Classes/4x/class.tx_causalaccounts_synchronizationtask.php";s:4:"25e4";s:37:"Classes/4x/class.ux_tx_openid_sv1.php";s:4:"d13b";s:36:"Classes/Controller/EidController.php";s:4:"9ada";s:36:"Classes/Task/SynchronizationTask.php";s:4:"b0d3";s:32:"Classes/Xclass/OpenidService.php";s:4:"6f40";s:26:"Documentation/Includes.txt";s:4:"df03";s:23:"Documentation/Index.rst";s:4:"c9d8";s:26:"Documentation/Settings.yml";s:4:"30a9";s:25:"Documentation/Targets.rst";s:4:"94c2";s:43:"Documentation/AdministratorManual/Index.rst";s:4:"4749";s:63:"Documentation/AdministratorManual/InstallingExtension/Index.rst";s:4:"e607";s:33:"Documentation/ChangeLog/Index.rst";s:4:"2776";s:46:"Documentation/Images/authentication-openid.png";s:4:"e90c";s:42:"Documentation/Images/extension-manager.png";s:4:"3901";s:31:"Documentation/Images/finger.png";s:4:"f98d";s:33:"Documentation/Images/overview.png";s:4:"28ed";s:36:"Documentation/Introduction/Index.rst";s:4:"9c31";s:37:"Documentation/KnownProblems/Index.rst";s:4:"5745";s:32:"Documentation/ToDoList/Index.rst";s:4:"b948";}',
+	'_md5_values_when_last_written' => 'a:25:{s:13:"composer.json";s:4:"faa6";s:16:"ext_autoload.php";s:4:"6e9c";s:21:"ext_conf_template.txt";s:4:"cdbd";s:12:"ext_icon.gif";s:4:"57bc";s:17:"ext_localconf.php";s:4:"580b";s:42:"Classes/4x/class.tx_causalaccounts_eid.php";s:4:"cb56";s:58:"Classes/4x/class.tx_causalaccounts_synchronizationtask.php";s:4:"5938";s:37:"Classes/4x/class.ux_tx_openid_sv1.php";s:4:"6a2d";s:36:"Classes/Controller/EidController.php";s:4:"0278";s:36:"Classes/Task/SynchronizationTask.php";s:4:"3b54";s:32:"Classes/Xclass/OpenidService.php";s:4:"0fb1";s:26:"Documentation/Includes.txt";s:4:"df03";s:23:"Documentation/Index.rst";s:4:"723c";s:26:"Documentation/Settings.yml";s:4:"408c";s:25:"Documentation/Targets.rst";s:4:"94c2";s:43:"Documentation/AdministratorManual/Index.rst";s:4:"4749";s:63:"Documentation/AdministratorManual/InstallingExtension/Index.rst";s:4:"e607";s:33:"Documentation/ChangeLog/Index.rst";s:4:"2776";s:46:"Documentation/Images/authentication-openid.png";s:4:"e90c";s:42:"Documentation/Images/extension-manager.png";s:4:"3901";s:31:"Documentation/Images/finger.png";s:4:"f98d";s:33:"Documentation/Images/overview.png";s:4:"28ed";s:36:"Documentation/Introduction/Index.rst";s:4:"9c31";s:37:"Documentation/KnownProblems/Index.rst";s:4:"5745";s:32:"Documentation/ToDoList/Index.rst";s:4:"b948";}',
 );
 
 ?>
