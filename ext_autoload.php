37095d281363c6170201031938e10761ed2e1877
Florian Wessels <f.wessels@bitmotion.de>
2018-02-06 11:01:34 +0100

    [TASK] Remove ext_autoload.php

--- ext_autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-$classMap = array();
-if (version_compare(TYPO3_version, '6.0.0', '<')) {
-    $extensionPath = t3lib_extMgm::extPath('causal_accounts');
-    $classMap = array(
-        'tx_causalaccounts_synchronizationtask' => $extensionPath . 'Classes/4x/class.tx_causalaccounts_synchronizationtask.php',
-    );
-}
-return $classMap;
